b265480b2e58400206212f748f343eea3e8b449f
Mike_S_D <9834088@gmail.com>
2020-10-26 23:58:14 +0300

    Updates

---
+++ precache-manifest.ea6a4bd5a3732970bbbf2e70a3921194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222edf54e5540149eb84a75462d2d500",
+    "url": "/spase_app/index.html"
+  },
+  {
+    "revision": "733feeac204e3fe30e84",
+    "url": "/spase_app/static/css/main.22547423.chunk.css"
+  },
+  {
+    "revision": "e370281280b111862246",
+    "url": "/spase_app/static/js/2.0208903d.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/spase_app/static/js/2.0208903d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733feeac204e3fe30e84",
+    "url": "/spase_app/static/js/main.c1fedc64.chunk.js"
+  },
+  {
+    "revision": "2060720db93cab5b5e07",
+    "url": "/spase_app/static/js/runtime-main.5c6930ef.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spase_app/static/media/logo.d5ab31ce.svg"
+  }
+]);
